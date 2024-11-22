1f730b0d8af0481ab2c2bd517ef5022822d95e18
João leandro Silva Sousa <183873216+leandro2638@users.noreply.github.com>
2024-11-22 16:44:00 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+export default function App() {
+  return(
+    <div ClassName="App">
+      <img src="./react.png">
+      </img>
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuário web e nativas.</p>
+      <button>Aprenda React</button>
+      <button>Referéncia da API</button>
+      
+      <hr/>
+      
+      <h1>Crie interfaces de usuário de componentes</h1>
+      <p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes</p>
+      
+      <hr/>
+      
+      <h1>Escreva componentes com código e marcação</h1>
+      <p>Componentes React são funções JavaScript. A sintexe de marcação é chamada de JSX.E uma extenção da sintexe JavaScript popularizada pelo React</p>
+      
+      <hr/>
+      
+      <h1>Próximos passos</h1>
+      <ul>
+      <li>Uso de dados dinâmicos no JSX</li>
+      <li>Criação de novos componentes</li>
+      <li>Estilização de componentes</li>
+      <li>Reutilização de componentes</li>
+      <li>Uso de props e children</li>
+      <li>Uso de eventos de JavaScript</li>
+      </ul>
+    </div>
+  )
+}
